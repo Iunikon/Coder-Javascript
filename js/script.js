456b6e8e28f5576c59dc9b0b1d036c64089bbe58
Jorge Miguel Pérez Roblez <jorgemiguelperezroblez@gmail.com>
2024-01-15 23:56:10 -0600

    Clase 1

---
+++ js/script.js
@@ -0,0 +1,22 @@
+let platoprincipal = 1000;
+let bebida = 400;
+let total = platoprincipal + bebida;
+
+let dobledeprecio = platoprincipal * 2;
+let porciones = 2;
+let personas = 3;
+
+let preciototal = platoprincipal * porciones;
+
+let dividircostos = preciototal / personas;
+
+let descuento = 100;
+
+let preciocondescuento = preciototal - descuento;
+
+let nombre = prompt ("nombre");
+let apellido = prompt ("apellido");
+
+let nombrecompleto = nombre +" "+ apellido;
+
+console.log ("Bienvenido", nombrecompleto);
